6f00a37047730e8ee4c96e6b6a2982907092602d
danielcmc100 <danielcmc100@gmail.com>
2025-05-17 16:31:39 -0300

    feat: add onMount lifecycle method to log page load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    console.log('Page loaded');
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<img src="/favicon.png" alt="Svelte logo" width="200" height="200 " />
+
